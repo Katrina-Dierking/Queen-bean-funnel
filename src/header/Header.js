6035bacc4338eac147781e287e782e83e05effeb
Katrina <kalynndier@gmail.com>
2023-03-22 21:23:39 -0400

    header created

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './header.css'
+import bee from './bee.png'
 
 function Header() {
   return (
     <header>
-        <img src="#" alt = "yellow banner with beehives along top of page"></img>
+      <section className="headerOne">
+        <img src={bee} />
+        <h2>QUEEN BEAN DESIGN CAFE</h2>
+      </section>
+    <section className="headerTwo">
+      <input type="text" placeholder="Enter your email"></input>
+      <button type="submit">Yes! I want in!</button>
+    </section>
     </header>
-  )
+  );
 }
 
 export default Header
