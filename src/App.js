f0fa71eebbfb1a22babbda2acb7cafd8c35076f5
Katrina <kalynndier@gmail.com>
2023-03-23 11:33:09 -0400

    main added. Styling continued

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Main from './main/Main';
 import './app.css'
 import Header from './header/Header';
 import Middle from './middle/Middle';
+import Bee from './lotties/Bee';
+
+
 
 function App() {
   return (
     <section>
-    <Header />
-    <Main />
-    <Middle />
+      <Header />
+      <Bee />
+      <Main />
+      <Middle />
     </section>
   );
 }
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import './main.css'
+import headshot from '../myHeadshot.png'
 
 function Main() {
   return (
    <main>
-        <section className='leftMain'>
-            <img src="#" />
-            <h3>Queen Bean Design Cafe</h3>
-        </section>
-        <section className='rightMain'>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus deserunt qui illo accusantium corrupti impedit numquam, similique laborum nisi quaerat, aliquam magni distinctio neque expedita nam, id obcaecati eius laudantium?</p>
-            <img src="#" />
-        </section>
+    <section className='mainInsert'>
+    
+      <section className='leftMain'>
+          <img src={headshot}  className="headshot" alt="Katrina Dierking headshot"/>
+          <h2>KATRINA DIERKING</h2>
+          <div className='icons'>
+
+          <p>SOFTWARE ENGINEER </p>
+          </div>
+      </section>
+      <section className='rightMain'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus deserunt qui illo accusantium corrupti impedit numquam, similique laborum nisi quaerat, aliquam magni distinctio neque expedita nam, id obcaecati eius laudantium?</p>
+          <img src="#" />
+      </section>
+    </section>
    </main>
   )
 }
