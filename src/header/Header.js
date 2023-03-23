92a73fbe2c14721d4388a997423dc075305b0a1c
Katrina <kalynndier@gmail.com>
2023-03-23 08:24:17 -0400

    middle section updated

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,18 +2,19 @@ import React from 'react'
 import './header.css'
 import bee from './bee.png'
 import coffee from './coffee.png'
+import logo from './QueenBeanLogo.png'
 
 function Header() {
   return (
     <header>
       <section className="headerOne">
-      <h2>QUEEN BEAN DESIGN CAFE</h2>
-      <img src={coffee} className="coffee"/>
+        <h2>QUEEN BEAN DESIGN CAFE</h2>
+        <img src={logo} className="coffee"/>
       </section>
       <section className="headerTwo">
-      <input type="text" placeholder="Enter your email"></input>
-      <img src={bee} className="bee"/>
-      <button type="submit">Yes! I want in!</button>
+        <input type="text" placeholder="Enter your email"></input>
+        <img src={bee} className="headerBee"/>
+        <button type="submit">Yes! I want in!</button>
     </section>
     </header>
   );
--- src/middle/Middle.js
+++ src/middle/Middle.js
@@ -3,45 +3,53 @@ import './middle.css'
 import crafter from './crafter.jpg'
 import business from './business.jpg'
 import homeschooler from './homeschooler.jpg'
+import bee from "./bee.png";
+
+
 function Middle() {
   return (
-    <section className="middle">
-      <section className=" categories leftMiddle">
-        <img src={crafter} alt="crafter" />
-        <h2>CRAFTER</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-          delectus? Incidunt adipisci iusto illo eligendi dolor laborum
-          provident maxime magnam, eius, similique odio, pariatur veniam vitae
-          distinctio eum tempora sed?
-        </p>
-        <button className="button"> - FIND OUT MORE</button>
-      </section>
+    <>
+    <section className="middleBack">
+      <section className="middle">
+        <section className=" categories leftMiddle">
+          <img src={crafter} alt="crafter" />
+          <h2>CRAFTER</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            delectus? Incidunt adipisci iusto illo eligendi dolor laborum
+            provident maxime magnam, eius, similique odio, pariatur veniam vitae
+            distinctio eum tempora sed?
+          </p>
+          <button className="button"> - FIND OUT MORE</button>
+        </section>
 
-      <section className=" categories middleMiddle">
-        <img src={homeschooler} alt="homeschooler" />
-        <h2>HOMESCHOOLER</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-          delectus? Incidunt adipisci iusto illo eligendi dolor laborum
-          provident maxime magnam, eius, similique odio, pariatur veniam vitae
-          distinctio eum tempora sed?
-        </p>
-        <button className="button"> - FIND OUT MORE</button>
-      </section>
+        <section className=" categories middleMiddle">
+          <img src={homeschooler} alt="homeschooler" />
+          <h2>HOMESCHOOLER</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            delectus? Incidunt adipisci iusto illo eligendi dolor laborum
+            provident maxime magnam, eius, similique odio, pariatur veniam vitae
+            distinctio eum tempora sed?
+          </p>
+          <button className="button"> - FIND OUT MORE</button>
+        </section>
 
-      <section className=" categories rightMiddle">
-        <img src={business} alt="business owner" />
-        <h2>BUSINESS OWNER</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-          delectus? Incidunt adipisci iusto illo eligendi dolor laborum
-          provident maxime magnam, eius, similique odio, pariatur veniam vitae
-          distinctio eum tempora sed?
-        </p>
-        <button className="button"> - FIND OUT MORE</button>
-      </section>
-    </section>
+        <section className=" categories rightMiddle">
+          <img src={business} alt="business owner" />
+          <h2>BUSINESS OWNER</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            delectus? Incidunt adipisci iusto illo eligendi dolor laborum
+            provident maxime magnam, eius, similique odio, pariatur veniam vitae
+            distinctio eum tempora sed?
+          </p>
+          <button className="button"> - FIND OUT MORE</button>
+          </section>
+        </section>
+        </section>
+        <img src={bee} className="bee" />
+        </>
   );
 }
 
