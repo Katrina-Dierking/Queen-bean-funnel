47afb1cf04d9ff0c303b3b3ad29a38ec740d7846
Katrina <kalynndier@gmail.com>
2023-03-24 07:09:31 -0400

    favicon and title changed

--- src/bottom/Bottom.js
+++ src/bottom/Bottom.js
@@ -7,6 +7,7 @@ function Bottom() {
     <section className="bottom">
       <h3>
         Designing the family, life, and/or business you've always wanted is closer than you think. 
+        <p>Let's build something great together.</p>
       </h3>
       <button className="button bot"> - SCHEDULE A DISCOVERY CALL TODAY</button>
       </section>
