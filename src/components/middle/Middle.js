06cc35674b093afb0271d618d1ee5ec3cc91d554
Katrina <kalynndier@gmail.com>
2023-05-09 11:38:05 -0400

    info section

--- src/components/middle/Middle.js
+++ src/components/middle/Middle.js
@@ -16,34 +16,39 @@ function Middle() {
       <section className="middleBack">
         <section className="middle">
           <section className=" categories leftMiddle">
-            <HiveChoice 
-             img = {crafter} alt="crafter" 
-             title = "CREATE"
-             para="
+            <HiveChoice
+              img={crafter}
+              alt="crafter"
+              title="CREATE"
+              para="
                Launching a home-based crafting business is hard work, but so worth it. Not sure where to get started? Join the
                Queen Bean Creation-Hive
                to get new business ideas, learn how to design/create/produce a wide range of hand-made products, and get the mentoring you need to make your dreams a reality.
              "
-             text = "FIND OUT MORE"
+              text="FIND OUT MORE"
+              link="https://blog.queenbeandesigncafe.com"
             />
           </section>
 
+   
           <section className=" categories middleMiddle">
-            <HiveChoice 
-            img ={business} alt="business owner"
-            title="CODE"
-            para="Are you a new developer looking for a place to code with other new devs? Are you a dev who has been effected by the recent layoffs? Are you looking for a meaningful project to contribute to and use as experience? Queen Bean Code Hive has a place for you. Contribute, learn, mentor others. Sharpen your skills while getting real-world experience."
-            text="FIND OUT MORE"
-            />
-          </section>
+              <HiveChoice
+                img={business}
+                alt="business owner"
+                title="CODE"
+                para="Are you a new developer looking for a place to code with other new devs? Are you a dev who has been effected by the recent layoffs? Are you looking for a meaningful project to contribute to and use as experience? Queen Bean Code Hive has a place for you. Contribute, learn, mentor others. Sharpen your skills while getting real-world experience."
+                text="FIND OUT MORE"
+              />
+              </section>
+           
 
           <section className=" categories rightMiddle">
-            <HiveChoice 
-            img ={homeschooler} alt="homeschooler"
-            title="COLLABORATE"
-            para="Are you new to homeschooling and need a community built just for other homeschooling moms in your shoes? Queen Bean Homeschooling Mom Hive is a social app built just for homeschoolers. Find virtual classes for your students run by other homeschooling moms, or teach a class yourself. Buy/Sell resources. Enjoy a monthly virtual Moms' Night Out and more."
-            text="FIND OUT MORE"
-            
+            <HiveChoice
+              img={homeschooler}
+              alt="homeschooler"
+              title="COLLABORATE"
+              para="Are you new to homeschooling and need a community built just for other homeschooling moms in your shoes? Queen Bean Homeschooling Mom Hive is a social app built just for homeschoolers. Find virtual classes for your students run by other homeschooling moms, or teach a class yourself. Buy/Sell resources. Enjoy a monthly virtual Moms' Night Out and more."
+              text="FIND OUT MORE"
             />
           </section>
         </section>
--- src/modals/HiveChoice.js
+++ src/modals/HiveChoice.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './hiveChoice.scss'
 
-const HiveChoice = ({title, img, alt, para, extraText, text}) => {
+const HiveChoice = ({title, img, alt, para, link,text}) => {
   return (
-    <section className='hiveChoice'>
-        <img src={img} alt = {alt}/>
-        <h2>{title}</h2>
-        <p>{para}</p>
-        <button className='button'>{text}</button>
+    <section className="hiveChoice">
+      <img src={img} alt={alt} />
+      <h2>{title}</h2>
+      <p>{para}</p>
+      <Link to={link} target="_blank">
+        <button className="hivebutton">{text}</button>
+      </Link>
     </section>
-  )
+  );
 }
 
 export default HiveChoice
