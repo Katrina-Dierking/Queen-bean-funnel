426f0760fbfbb56f48fb0106cc1406ee3b1517da
Katrina <kalynndier@gmail.com>
2023-05-09 12:17:25 -0400

    info style

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -7,8 +7,25 @@ const Info = () => {
       <div className="cont">
         <h3>Are you ready to turn tomorrow's dreams into today's reality?</h3>
         <p>
-          Hi, I'm Katrina Dierking, founder of <span>Queen Bean Design Cafe</span> where if you can dream it, we can
+          Hi, I'm Katrina Dierking, founder of
+          <span> Queen Bean Design Cafe</span> where if you can dream it, we can
           design it and make it a reality.
+          <p>Whether it's:</p>
+          <ul>
+            <li>
+              {" "}
+              <span className='create'>creating</span> ideas for a homebased business
+            </li>
+            <li>
+              a place to <span className='code'>code </span>along and break into the tech
+              industry
+            </li>
+            <li>
+              homeschooling community full of resources and{" "}
+              <span className='collab'>collaboration</span>
+            </li>
+          </ul>
+          we've got you covered.
         </p>
       </div>
     </section>
